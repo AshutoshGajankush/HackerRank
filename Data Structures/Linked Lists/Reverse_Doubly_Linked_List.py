63831d11ec303dba1781de9c171989c16cbdb591
Ashutosh Gajankush <agajanku@stevens.edu>
2016-11-13 17:12:33 -0500

    Reverse_Doubly_linked_list

---
+++ Data Structures/Linked Lists/Reverse_Doubly_Linked_List.py
@@ -0,0 +1,29 @@
+"""
+ Reverse a doubly linked list
+ head could be None as well for empty list
+ Node is defined as
+ 
+ class Node(object):
+ 
+   def __init__(self, data=None, next_node=None, prev_node = None):
+       self.data = data
+       self.next = next_node
+       self.prev = prev_node
+
+ return the head node of the updated list 
+"""     
+def Reverse(head):
+    temp = head
+    newHead = head
+    while(temp!=None):
+        temp.prev, temp.next = temp.next, temp.prev # Swaping next and prev elemenets
+        newHead = temp
+        temp = temp.prev
+    return newHead
+  
+  
+  
+  
+  
+  
+  

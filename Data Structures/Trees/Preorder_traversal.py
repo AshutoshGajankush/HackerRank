71ab9d0adf75db6960c7396f668d7a12a0d38cfc
Ashutosh Gajankush <agajanku@stevens.edu>
2016-11-15 23:08:00 -0500

    Trees

---
+++ Data Structures/Trees/Preorder_traversal.py
@@ -0,0 +1,13 @@
+"""
+Node is defined as
+self.left (the left child of the node)
+self.right (the right child of the node)
+self.data (the value of the node)
+"""
+def preOrder(root):
+    #Write your code here
+    print root.data,
+    if (root.left != None):
+        preOrder(root.left)
+    if(root.right != None):
+        preOrder(root.right)
